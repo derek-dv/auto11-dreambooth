54cc7018717374b09a8e82b825aeaecffab4e27f
Derek David <derekmiracledavid@gmail.com>
2023-05-27 12:28:40 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, UploadFile, File
+import os
+import zipfile
+import magic
+from train import train
+app = FastAPI()
+
+
+def is_zip_file(file_contents):
+    mime_type = magic.from_buffer(file_contents, mime=True)
+    print(mime_type)
+    return mime_type == 'application/zip'
+
+
+class NotZipException(Exception):
+    pass
+
+
+def unzip_file(zip_path, extract_path):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/train")
+async def train_dreamooth(model_name: str, instance_prompt: str, file: UploadFile = File(...)):
+    try:
+        file_content = await file.read()
+        if not os.path.exists("test123"):
+            os.makedirs("datasets")
+        if not is_zip_file(file_content):
+            raise NotZipException("File uploaded not zip")
+        with open(f"datasets/{file.filename}", "wb") as f:
+            f.write(file_content)
+        unzip_file(f"datasets/{file.filename}", f"datasets/{model_name}")
+        is_training = train(model_name=model_name, is_new_model=True)
+
+        return {"filename": file.filename}
+    except FileExistsError:
+        return {"message": "File exists"}
+    except NotZipException:
+        return {"message": "File uploaded not zip"}
---
+++ train.py
@@ -0,0 +1,46 @@
+import requests
+import json
+
+
+def train(url: str, model_name: str = "Test", is_new_model: bool = True, class_prompt="", instance_propmpt="", scheduler: str = "DEISMultistep", src_model: str = "/workspace/stable-diffusion-webui/models/Stable-diffusion/rose_del_me/rose_del_me_1800.safetensors"):
+    # Create model
+    if is_new_model:
+        create_endpoint = f"{url}/dreambooth/createModel?new_model_name={model_name}&new_model_src={src_model}&is_512=true&new_model_extract_ema=false&new_model_scheduler={scheduler}"
+        print(create_endpoint)
+        create_res = requests.post(create_endpoint)
+        text = create_res.text
+        print(text)
+
+        with open("sample_config.json", "r") as f:
+            txt = f.read()
+            config = json.loads(txt)
+        config["model_name"] = model_name
+        config["model_dir"] = f"/workspace/stable-diffusion-webui/models/dreambooth/{model_name}"
+        config["model_path"] = f"/workspace/stable-diffusion-webui/models/dreambooth/{model_name}"
+        config[
+            "pretrained_model_name_or_path"] = f"/workspace/stable-diffusion-webui/models/dreambooth/{model_name}/working"
+        config["concepts_list"][0]["class_prompt"] = class_prompt
+        config["concepts_list"][0]["instance_data_dir"] = f"dataset{model_name}"
+        config["concepts_list"][0]["instance_prompt"] = instance_propmpt
+
+        config_endpoint = f"{url}/dreambooth/model_config"
+        config_res = requests.post(
+            config_endpoint,
+            json=config
+        )
+        print("Config added")
+        print(config_res.text)
+        train_endpoint = f'{url}/dreambooth/start_training?model_name={model_name}&use_tx2img=false'
+        train_res = requests.post(train_endpoint)
+        print(train_res.text)
+        if train_res.status_code == 200:
+            return True
+        return False
+
+        # Add config
+
+
+if (__name__ == "__main__"):
+
+    # print(config)
+    train("https://cbb77c41d86fd57d33.gradio.live")
